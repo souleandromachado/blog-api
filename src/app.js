71dc9226141360af756b85ab672d23710a1aefe5
Leandro Machado <leandrsxe@icloud.com>
2025-03-18 15:08:00 -0300

    Ajustado o CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
