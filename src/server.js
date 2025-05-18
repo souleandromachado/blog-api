4f44f1c3fd09b5d399e89b05a8dc43cafaa91119
Leandro Machado <leandro@MacBook-Air-de-Leandro.local>
2025-05-17 23:35:13 -0300

    ajuste da porta exposta para o render

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000 
 
-const PORT = 3000;
+//const PORT = 3000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
